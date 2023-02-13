5fdc55949ceb80098d1006b8248e5503a05413b8
ronwthompson <moliviank@hotmail.com>
2023-02-12 18:56:40 -0800

    cleanup and extra credit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# canvasing-back
+# Getting started
+
+You'll need a few tools to setup this project:
+
+- PostgreSQL [https://www.postgresql.org/download/](Installation Instructions)
+- Node v18+ [https://nodejs.org/en/download/](Installation Instructions)
+- pgAdmin v4+ [https://www.pgadmin.org/download/](Installation Instructions) (optional)
+
+Once those are installed, run PostgreSQL locally and create a database in your local PostgreSQL instance. I've created one called "canvasing" but you can name it anything you'd like.
+
+Either using pgAdmin or the command-line tool `psql`, connect to your local database and create the table this app will use by running the SQL code in `migrations/setup.sql`. This should create a table called `notes_table` with 4 columns (id, name, email, and notes).
+
+Update the database's credentials in `src/app.js` with your user, host, database name, and password.
+
+Navigate to the project directory and run `node src/app.js`
+
+This will run the app in development mode.
+
+Open [http://localhost:3001/healthCheck](http://localhost:3001/healthCheck) in your favorite browser to ensure the app is running correctly.
+
+You will also need to download and run the frontend, which is located [https://github.com/ronwthompson/canvasing-front](here).
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 const hostname = '127.0.0.1';
 const port = 3001;
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
 
 const { Client } = require("pg");
 const credentials = { // be sure to edit this object with your credentials!
@@ -14,42 +14,46 @@ const credentials = { // be sure to edit this object with your credentials!
   database: "canvasing",
   password: "",
   port: 5432,
-}
+};
 
-app.use(cors())
+app.use(cors());
+
+app.get('/healthCheck', jsonParser, (req, res) => {
+  res.send('Everything is looking good!');
+});
 
 app.post('/saveNote', jsonParser, (req, res) => {
-  const client = new Client(credentials)
+  const client = new Client(credentials);
   client.connect();
 
   client.query(`
     INSERT INTO notes_table (id, name, notes, email) 
     values (gen_random_uuid(), '${req.body.name}', '${req.body.notes}', '${req.body.email}')`)
     .then(results => {
-      res.send(results)
+      res.send(results);
     })
     .catch(err => {
-      throw err
+      throw err;
     })
     .then(() => {
-      client.end()
-    })
-})
+      client.end();
+    });
+});
 
 app.get('/viewNotes', jsonParser, (req, res) => {
-  const client = new Client(credentials)
+  const client = new Client(credentials);
   client.connect();
   client.query(`SELECT * from notes_table`)
     .then(results => {
-      res.send(results.rows)
+      res.send(results.rows);
     })
     .catch(err => {
-      throw err
+      throw err;
     })
     .then(() => {
-      client.end()
-    })
-})
+      client.end();
+    });
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
